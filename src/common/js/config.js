3458f7bcab9b726b91b7026b44f277b4299d3387
kangyilong <1603049109@qq.com>
2019-05-30 16:26:17 +0800

    系统编号

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,4 +1,5 @@
-export const SYSTEM_CODE = 'CD-OGC000021';
+// export const SYSTEM_CODE = 'CD-OGC000021'; // 系统编号
+export const SYSTEM_CODE = 'CL-TICP';
 // 七牛上传地址
 export const UPLOAD_URL = 'http://up.qiniup.com';
 // 七牛图片前缀
