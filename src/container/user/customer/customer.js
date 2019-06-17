fb396d464365c424b251ea6bef13868f89b8ddf7
wulq <wlqsltz@163.com>
2019-06-17 17:49:00 +0800

    新增修改用户等级

--- src/container/user/customer/customer.js
+++ src/container/user/customer/customer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Modal} from 'antd';
+import { Modal, Select, message } from 'antd';
 import {
     setTableData,
     setPagination,
@@ -10,9 +10,12 @@ import {
     cancelFetching,
     setSearchData
 } from '@redux/user/customer/customer';
-import {listWrapper} from 'common/js/build-list';
-import {dateTimeFormat, showWarnMsg, showSucMsg} from 'common/js/util';
-import {activateUser, setQ} from 'api/user';
+import { listWrapper } from 'common/js/build-list';
+import { dateTimeFormat, showWarnMsg, showSucMsg, isUndefined } from 'common/js/util';
+import { activateUser, setQ } from 'api/user';
+import fetch from 'common/js/fetch';
+
+const { Option } = Select;
 
 @listWrapper(
     state => ({
@@ -25,7 +28,15 @@ import {activateUser, setQ} from 'api/user';
     }
 )
 class Customer extends React.Component {
+    state = {
+        levelVal: '',
+        levelVisible: false
+    };
+    levelChagne = levelVal => {
+       this.setState({ levelVal });
+    }
     render() {
+        const { levelVisible, levelVal, userId } = this.state;
         const fields = [{
             field: 'nickname',
             title: '昵称',
@@ -109,122 +120,183 @@ class Customer extends React.Component {
             field: 'remark',
             title: '备注'
         }];
-        return this.props.buildList({
-            fields,
-            rowKey: 'userId',
-            pageCode: '805120',
-            singleSelect: false,
-            searchParams: {
-            },
-            btnEvent: {
-                active: (selectedRowKeys, selectedRows) => {
-                    if (!selectedRowKeys.length) {
-                        showWarnMsg('请选择记录');
-                    } else {
-                        let userIdList = [];
-                        for(let i = 0, len = selectedRows.length; i < len; i++) {
-                            if(selectedRows[i].status === '0') {
-                                showWarnMsg(selectedRows[i].nickname + '用户已是正常状态');
-                                userIdList = [];
-                                return;
-                            }
-                            userIdList.push(selectedRows[i].userId);
-                        }
-                        if (userIdList.length > 0) {
-                            Modal.confirm({
-                                okText: '确认',
-                                cancelText: '取消',
-                                content: `确定允许用户登录？`,
-                                onOk: () => {
-                                    this.props.doFetching();
-                                    return activateUser(userIdList).then(() => {
-                                        this.props.getPageData();
-                                        showSucMsg('操作成功');
-                                    }).catch(() => {
-                                        this.props.cancelFetching();
+        return (
+            <div>
+            {
+                this.props.buildList({
+                    fields,
+                    rowKey: 'userId',
+                    pageCode: '805120',
+                    singleSelect: false,
+                    searchParams: {
+                    },
+                    btnEvent: {
+                        active: (selectedRowKeys, selectedRows) => {
+                            if (!selectedRowKeys.length) {
+                                showWarnMsg('请选择记录');
+                            } else {
+                                let userIdList = [];
+                                for(let i = 0, len = selectedRows.length; i < len; i++) {
+                                    if(selectedRows[i].status === '0') {
+                                        showWarnMsg(selectedRows[i].nickname + '用户已是正常状态');
+                                        userIdList = [];
+                                        return;
+                                    }
+                                    userIdList.push(selectedRows[i].userId);
+                                }
+                                if (userIdList.length > 0) {
+                                    Modal.confirm({
+                                        okText: '确认',
+                                        cancelText: '取消',
+                                        content: `确定允许用户登录？`,
+                                        onOk: () => {
+                                            this.props.doFetching();
+                                            return activateUser(userIdList).then(() => {
+                                                this.props.getPageData();
+                                                showSucMsg('操作成功');
+                                            }).catch(() => {
+                                                this.props.cancelFetching();
+                                            });
+                                        }
                                     });
                                 }
-                            });
-                        }
-                    }
-                },
-                rock: (selectedRowKeys, selectedRows) => {
-                    if (!selectedRowKeys.length) {
-                        showWarnMsg('请选择记录');
-                    } else {
-                        let userIdList = [];
-                        for(let i = 0, len = selectedRows.length; i < len; i++) {
-                            if(selectedRows[i].status === '2') {
-                                showWarnMsg(selectedRows[i].nickname + '用户已禁止登录');
-                                userIdList = [];
-                                return;
                             }
-                            userIdList.push(selectedRows[i].userId);
-                        }
-                        if (userIdList.length > 0) {
-                            Modal.confirm({
-                                okText: '确认',
-                                cancelText: '取消',
-                                content: `确定禁止用户登录？`,
-                                onOk: () => {
-                                    this.props.doFetching();
-                                    return activateUser(userIdList).then(() => {
-                                        this.props.getPageData();
-                                        showSucMsg('操作成功');
-                                    }).catch(() => {
-                                        this.props.cancelFetching();
+                        },
+                        rock: (selectedRowKeys, selectedRows) => {
+                            if (!selectedRowKeys.length) {
+                                showWarnMsg('请选择记录');
+                            } else {
+                                let userIdList = [];
+                                for(let i = 0, len = selectedRows.length; i < len; i++) {
+                                    if(selectedRows[i].status === '2') {
+                                        showWarnMsg(selectedRows[i].nickname + '用户已禁止登录');
+                                        userIdList = [];
+                                        return;
+                                    }
+                                    userIdList.push(selectedRows[i].userId);
+                                }
+                                if (userIdList.length > 0) {
+                                    Modal.confirm({
+                                        okText: '确认',
+                                        cancelText: '取消',
+                                        content: `确定禁止用户登录？`,
+                                        onOk: () => {
+                                            this.props.doFetching();
+                                            return activateUser(userIdList).then(() => {
+                                                this.props.getPageData();
+                                                showSucMsg('操作成功');
+                                            }).catch(() => {
+                                                this.props.cancelFetching();
+                                            });
+                                        }
                                     });
                                 }
-                            });
+                            }
+                        },
+                        // 修改广告费率
+                        editAdvertisementFee: (selectedRowKeys, selectedRows) => {
+                            if (!selectedRowKeys.length) {
+                                showWarnMsg('请选择记录');
+                            } else if (selectedRowKeys.length > 1) {
+                                showWarnMsg('请选择一条记录');
+                            } else {
+                                this.props.history.push(`/user/customer/editAdvertisementFee?code=${selectedRowKeys[0]}`);
+                            }
+                        },
+                        // 账户查询
+                        accountQuery: (selectedRowKeys, selectedRows) => {
+                            if (!selectedRowKeys.length) {
+                                showWarnMsg('请选择记录');
+                            } else if (selectedRowKeys.length > 1) {
+                                showWarnMsg('请选择一条记录');
+                            } else {
+                                this.props.history.push(`/user/customer/accountQuery?userId=${selectedRowKeys[0]}`);
+                            }
+                        },
+                        // 新增节点用户
+                        addNode: (selectedRowKeys, selectedRows) => {
+                            if (!selectedRowKeys.length) {
+                                showWarnMsg('请选择记录');
+                            } else if (selectedRowKeys.length > 1) {
+                                showWarnMsg('请选择一条记录');
+                            } else if(selectedRows[0].nodeLevel) {
+                                showWarnMsg('该用户已是节点用户');
+                            } else {
+                                this.props.history.push(`/user/customer/userNode?userId=${selectedRowKeys[0]}`);
+                            }
+                        },
+                        // 修改节点用户
+                        editNode: (selectedRowKeys, selectedRows) => {
+                            if (!selectedRowKeys.length) {
+                                showWarnMsg('请选择记录');
+                            } else if (selectedRowKeys.length > 1) {
+                                showWarnMsg('请选择一条记录');
+                            } else if(!selectedRows[0].nodeLevel) {
+                                showWarnMsg('该用户还不是节点用户');
+                            } else {
+                                this.props.history.push(`/user/customer/userNode?userId=${selectedRowKeys[0]}&nodeLevel=1`);
+                            }
+                        },
+                        editLevel: (selectedRowKeys, selectedRows) => {
+                            if (!selectedRowKeys.length) {
+                                showWarnMsg('请选择记录');
+                            } else if (selectedRowKeys.length > 1) {
+                                showWarnMsg('请选择一条记录');
+                            } else {
+                                this.setState({
+                                    levelVisible: true,
+                                    userId: selectedRows[0].userId,
+                                    levelVal: selectedRows[0].gplhNodeLevel
+                                });
+                            }
                         }
                     }
-                },
-                // 修改广告费率
-                editAdvertisementFee: (selectedRowKeys, selectedRows) => {
-                    if (!selectedRowKeys.length) {
-                        showWarnMsg('请选择记录');
-                    } else if (selectedRowKeys.length > 1) {
-                        showWarnMsg('请选择一条记录');
-                    } else {
-                        this.props.history.push(`/user/customer/editAdvertisementFee?code=${selectedRowKeys[0]}`);
-                    }
-                },
-                // 账户查询
-                accountQuery: (selectedRowKeys, selectedRows) => {
-                    if (!selectedRowKeys.length) {
-                        showWarnMsg('请选择记录');
-                    } else if (selectedRowKeys.length > 1) {
-                        showWarnMsg('请选择一条记录');
-                    } else {
-                        this.props.history.push(`/user/customer/accountQuery?userId=${selectedRowKeys[0]}`);
-                    }
-                },
-                // 新增节点用户
-                addNode: (selectedRowKeys, selectedRows) => {
-                    if (!selectedRowKeys.length) {
-                        showWarnMsg('请选择记录');
-                    } else if (selectedRowKeys.length > 1) {
-                        showWarnMsg('请选择一条记录');
-                    } else if(selectedRows[0].nodeLevel) {
-                        showWarnMsg('该用户已是节点用户');
-                    } else {
-                        this.props.history.push(`/user/customer/userNode?userId=${selectedRowKeys[0]}`);
-                    }
-                },
-                // 修改节点用户
-                editNode: (selectedRowKeys, selectedRows) => {
-                    if (!selectedRowKeys.length) {
-                        showWarnMsg('请选择记录');
-                    } else if (selectedRowKeys.length > 1) {
-                        showWarnMsg('请选择一条记录');
-                    } else if(!selectedRows[0].nodeLevel) {
-                        showWarnMsg('该用户还不是节点用户');
-                    } else {
-                        this.props.history.push(`/user/customer/userNode?userId=${selectedRowKeys[0]}&nodeLevel=1`);
-                    }
-                }
+                })
             }
-        });
+            <Modal
+              title="修改节点等级"
+              visible={levelVisible}
+              okText={'确定'}
+              cancelText={'取消'}
+              onOk={() => {
+                  if (isUndefined(levelVal)) {
+                      showWarnMsg('请选择节点等级');
+                      return;
+                  }
+                  let hasMsg = message.loading('');
+                  fetch('805074', {
+                      userId: userId,
+                      candyNodeLevel: levelVal
+                  }).then(() => {
+                      hasMsg();
+                      message.success('操作成功', 1, () => {
+                          this.setState({
+                              levelVisible: false
+                          });
+                          this.props.getPageData();
+                      });
+                  }, hasMsg);
+              }}
+              onCancel={() => {
+                  this.setState({
+                      levelVisible: false
+                  });
+              }}
+            >
+                <div>
+                    <label>节点等级：</label>
+                    <Select placeholder="请选择节点等级" value={levelVal} onChange={this.levelChagne} style={{ width: '60%' }}>
+                    {
+                        this.props.searchData.gplhNodeLevel
+                            ? this.props.searchData.gplhNodeLevel.map(v => (
+                                <Option value={v.dkey} key={v.dkey}>{v.dvalue}</Option>
+                            )) : null
+                    }
+                    </Select>
+                </div>
+            </Modal>
+            </div>
+        );
     }
 }
 
