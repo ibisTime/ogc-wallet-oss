8e778a4ec65a9bd667d352bb5b77dc24d3014e5f
haiqingzheng <leo.zheng@hichengdai.com>
2019-06-27 00:23:52 +0800

    logo样式调整，里面的

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -109,7 +109,7 @@ class Dashboard extends React.Component {
               this.props.setTopCode('');
               this.props.history.push('/');
           }}>
-              <img style ={{width: '72%', height: '40px'}} src={logo}/>
+              <img style ={{width: '30%'}} src={logo}/>
           </div>
         <Menu
           theme="dark"
